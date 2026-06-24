e3983f8fb9e9bb6158407574d448634e97ff4551
hoang <panel@hclou.local>
2026-06-24 05:22:49 +0000

    feat(db): them database.sql RONG du lieu (chi structure 11 bang + 1 admin: admin/admin123) + admin.sql rieng

---
+++ database.sql
@@ -0,0 +1,381 @@
+-- =====================================================================
+-- DATABASE PANEL - RONG DU LIEU (chi co cau truc + 1 admin moi)
+-- Tao tu file goc bang cach loai bo toan bo INSERT data cu
+-- Admin mac dinh:   admin / admin123  (level=1)
+-- Goi y: trong phpMyAdmin -> tao database moi -> Import file nay
+-- =====================================================================
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 26, 2026 at 10:38 AM
+-- Server version: 10.6.23-MariaDB
+-- PHP Version: 8.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sx2lador_xtff`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `credit`
+--
+
+CREATE TABLE `credit` (
+  `id` int(11) NOT NULL,
+  `name` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+--
+
+--
+-- Table structure for table `Feature`
+--
+
+CREATE TABLE `Feature` (
+  `id` int(11) NOT NULL,
+  `ESP` varchar(3) NOT NULL,
+  `Item` varchar(3) NOT NULL,
+  `SilentAim` varchar(3) NOT NULL,
+  `AIM` varchar(3) NOT NULL,
+  `BulletTrack` varchar(3) NOT NULL,
+  `Memory` varchar(3) NOT NULL,
+  `Floating` varchar(3) NOT NULL,
+  `Setting` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+--
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `id_history` int(11) NOT NULL,
+  `keys_id` varchar(33) DEFAULT NULL,
+  `user_do` varchar(33) DEFAULT NULL,
+  `info` mediumtext NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+--
+
+--
+-- Table structure for table `keys_code`
+--
+
+CREATE TABLE `keys_code` (
+  `id_keys` int(11) NOT NULL,
+  `game` varchar(32) NOT NULL,
+  `user_key` varchar(32) DEFAULT NULL,
+  `user_password` varchar(255) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `expired_date` datetime DEFAULT NULL,
+  `max_devices` int(11) DEFAULT NULL,
+  `devices` mediumtext DEFAULT NULL,
+  `status` tinyint(1) DEFAULT 1,
+  `registrator` varchar(32) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+--
+
+--
+-- Table structure for table `keys_system`
+--
+
+CREATE TABLE `keys_system` (
+  `id` int(11) NOT NULL,
+  `license_key` varchar(100) NOT NULL,
+  `days` int(11) NOT NULL,
+  `hwid` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lib`
+--
+
+CREATE TABLE `lib` (
+  `id` int(11) NOT NULL,
+  `file` varchar(255) NOT NULL,
+  `file_type` varchar(255) NOT NULL,
+  `file_size` varchar(32) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+--
+
+--
+-- Table structure for table `modname`
+--
+
+CREATE TABLE `modname` (
+  `id` int(11) NOT NULL,
+  `modname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+--
+
+--
+-- Table structure for table `onoff`
+--
+
+CREATE TABLE `onoff` (
+  `id` int(11) NOT NULL,
+  `status` varchar(5) NOT NULL,
+  `myinput` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+--
+
+--
+-- Table structure for table `referral_code`
+--
+
+CREATE TABLE `referral_code` (
+  `id_reff` int(11) NOT NULL,
+  `code` varchar(128) NOT NULL,
+  `Referral` varchar(7) NOT NULL,
+  `level` int(11) NOT NULL,
+  `set_saldo` int(11) NOT NULL DEFAULT 0,
+  `used_by` varchar(66) NOT NULL,
+  `created_by` varchar(66) NOT NULL DEFAULT 'Owner',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `acc_expiration` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+--
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_users` int(11) NOT NULL,
+  `fullname` varchar(155) DEFAULT NULL,
+  `username` varchar(66) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `reset_link_token` varchar(255) NOT NULL,
+  `exp_date` varchar(250) NOT NULL,
+  `level` int(11) NOT NULL,
+  `saldo` decimal(11,1) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT 1,
+  `uplink` varchar(66) DEFAULT NULL,
+  `password` varchar(155) NOT NULL,
+  `telegram_id` varchar(50) DEFAULT NULL,
+  `seller_key` varchar(100) DEFAULT NULL,
+  `user_ip` varchar(155) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `expiration_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+--
+
+--
+-- Table structure for table `_ftext`
+--
+
+CREATE TABLE `_ftext` (
+  `id` int(11) NOT NULL,
+  `_status` varchar(100) NOT NULL,
+  `_ftext` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+--
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `credit`
+--
+ALTER TABLE `credit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Feature`
+--
+ALTER TABLE `Feature`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`id_history`);
+
+--
+-- Indexes for table `keys_code`
+--
+ALTER TABLE `keys_code`
+  ADD PRIMARY KEY (`id_keys`),
+  ADD UNIQUE KEY `user_key` (`user_key`);
+
+--
+-- Indexes for table `keys_system`
+--
+ALTER TABLE `keys_system`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `license_key` (`license_key`);
+
+--
+-- Indexes for table `lib`
+--
+ALTER TABLE `lib`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `modname`
+--
+ALTER TABLE `modname`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `onoff`
+--
+ALTER TABLE `onoff`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `referral_code`
+--
+ALTER TABLE `referral_code`
+  ADD PRIMARY KEY (`id_reff`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_users`),
+  ADD UNIQUE KEY `username` (`username`,`email`);
+
+--
+-- Indexes for table `_ftext`
+--
+ALTER TABLE `_ftext`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `credit`
+--
+ALTER TABLE `credit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `Feature`
+--
+ALTER TABLE `Feature`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `history`
+--
+ALTER TABLE `history`
+  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4890;
+
+--
+-- AUTO_INCREMENT for table `keys_code`
+--
+ALTER TABLE `keys_code`
+  MODIFY `id_keys` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=319;
+
+--
+-- AUTO_INCREMENT for table `keys_system`
+--
+ALTER TABLE `keys_system`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lib`
+--
+ALTER TABLE `lib`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `modname`
+--
+ALTER TABLE `modname`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `onoff`
+--
+ALTER TABLE `onoff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `referral_code`
+--
+ALTER TABLE `referral_code`
+  MODIFY `id_reff` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=266;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=403;
+
+--
+-- AUTO_INCREMENT for table `_ftext`
+--
+ALTER TABLE `_ftext`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- =====================================================================
+-- TAI KHOAN ADMIN MAC DINH: admin / admin123
+-- =====================================================================
+INSERT INTO `users` (
+  `fullname`, `username`, `email`, `reset_link_token`, `exp_date`,
+  `level`, `saldo`, `status`, `uplink`, `password`,
+  `telegram_id`, `seller_key`, `user_ip`, `created_at`, `updated_at`, `expiration_date`
+) VALUES (
+  'HOANG', 'admin', 'admin@hclou.local', '', '2050-01-01 00:00:00',
+  1, 9999999.9, 1, 'SX2TEAM',
+  '$2y$08$kOhvFKwvt1VvRvo7lKhVO.FHcNH2edKLMXC9HD3TzzBd4yz7AWwqq',
+  NULL, 'seller_HCLOU2026admin0001abcdef0001abcd',
+  '127.0.0.1', NOW(), NOW(), '2050-01-01 00:00:00'
+);
+
+-- HET FILE --
